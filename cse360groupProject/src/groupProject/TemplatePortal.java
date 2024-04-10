e73dae1aaaa4b7cb8162ae28e926f196ae950c6e
adrianzhang1 <53829341+adrianzhang1@users.noreply.github.com>
2024-04-10 01:03:26 -0700

    templatePortal is my testing ground as of now for messagebox

--- cse360groupProject/src/groupProject/TemplatePortal.java
+++ cse360groupProject/src/groupProject/TemplatePortal.java
@@ -1,16 +1,66 @@
 package groupProject;
 
+import java.io.*;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 
 public class TemplatePortal extends Portal {
 	private Account user;
-	public Scene createPortal(Account newUser) {
-		this.user = newUser;
-		//HBox root = new HBox();
-		welcomeLbl.setText("Welcome Patient/Doctor/Nurse " + user.getFName());
+	
+	public Scene createPortal(Account newUser)  {
+		SystemDatabase database;
+		try {
+			FileInputStream fis = new FileInputStream("database.ser");
+		    ObjectInputStream ois = new ObjectInputStream(fis);
+		    database = (SystemDatabase) ois.readObject();
+		    ois.close();
+		    this.user = newUser;
+			//HBox root = new HBox();
+			welcomeLbl.setText("Welcome Patient/Doctor/Nurse " + user.getFName());
+			System.out.println(newUser.getUsername());
+			MessageBox box = new MessageBox(database.getMessages(newUser.getUsername()));
+			scene = new Scene(box.getBox(),1280,720);
+			box.getButton().setOnAction(new EventHandler<>() {
+	            public void handle(ActionEvent event) {
+	            	System.out.println("button");
+	            	database.submitMessage(newUser.getUsername(), newUser.getUsername(), box.getMessageSent());
+	            	
+	            	try {
+	            	FileOutputStream fos = new FileOutputStream("database.ser");
+	            	ObjectOutputStream oos = new ObjectOutputStream(fos);
+	            	oos.writeObject(database);
+	            	oos.close(); }
+	            	catch(FileNotFoundException e) {
+	        			// TODO Auto-generated catch block
+	        			e.printStackTrace();
+	        		} catch (IOException e) {
+	        			// TODO Auto-generated catch block
+	        			e.printStackTrace();
+	        		} 
+	            
+	            	
+	            	box.getMsgField().setText("");
+	            	box.createBox(database.getMessages(newUser.getUsername()));
+	            	scene = new Scene(box.getBox(),1280,720);
+	            }
+	        });
+		}
+		catch(FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		scene = new Scene(welcomeLbl,1280,720);
-		return scene;
+		
+		return scene;	
 	}
 }
