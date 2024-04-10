a9e60d5167d34b442f2dfb11079d76cc1eafea17
prototorp <114628972+prototorp@users.noreply.github.com>
2024-04-10 00:03:34 -0700

    Add files via upload

---
+++ createNewAccountPage.java
@@ -0,0 +1,152 @@
+package groupProject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+//TODO: make sure username is at least 3 characters no more than 32, no spaces
+// password: 1 character no more than 32
+
+public class createNewAccountPage {
+	private Scene scene;
+	private Button signUpButton;
+	private TextField usernameField;
+	private TextField firstNameField;
+	private TextField lastNameField;
+	private TextField emailField;
+	private TextField phoneNumberField;
+	private PasswordField passField;
+	private PasswordField confirmPassField;
+	private Label errMsg;
+	
+	public createNewAccountPage() throws IOException {
+		  Label heading = new Label("Tempe Pediatric Center");
+		  heading.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+	      heading.setTextFill(javafx.scene.paint.Color.RED);
+	      heading.setUnderline(true);
+	      
+	      Label emailLabel = new Label("Email");
+	      Label phoneNumberLabel = new Label("Phone Number");
+	      Label usernameLabel = new Label("Username");
+	      Label passLabel = new Label("Password");
+	      Label confirmPassLabel = new Label("Confirm Password");
+	      
+	      Label firstNameLabel = new Label("First Name");
+	      firstNameLabel.setAlignment(Pos.CENTER_LEFT);
+	      Label lastNameLabel = new Label("Last Name");
+	      lastNameLabel.setAlignment(Pos.CENTER_LEFT);
+	      
+	      emailField = new TextField();
+	      emailField.setPrefWidth(100);
+	      emailField.setPrefHeight(20);
+	      phoneNumberField = new TextField();
+	      phoneNumberField.setPrefWidth(100);
+	      phoneNumberField.setPrefHeight(20);
+	      usernameField = new TextField();
+	      firstNameField = new TextField();
+	      firstNameField.setPrefWidth(100);
+	      firstNameField.setPrefHeight(20);
+	      lastNameField = new TextField();
+	      lastNameField.setPrefWidth(100);
+	      lastNameField.setPrefHeight(20);
+	      passField = new PasswordField();
+	      confirmPassField = new PasswordField();
+	               
+	      Image image = new Image(new FileInputStream("C:\\Users\\conor\\eclipse-workspace\\project3Windows\\src\\project3Windows\\TPClogo.png"));
+	      ImageView imageView = new ImageView(image);
+	      imageView.setPreserveRatio(true);
+	      imageView.setFitWidth(200);
+	      imageView.setImage(image);
+	      
+	      VBox firstField = new VBox(5);
+	      firstField.getChildren().addAll(firstNameLabel, firstNameField);
+	      
+	      VBox lastField = new VBox(5);
+	      lastField.getChildren().addAll(lastNameLabel, lastNameField);
+	      HBox firstLastField = new HBox(50);
+	      firstLastField.getChildren().addAll(firstField,lastField);
+	      firstLastField.setAlignment(Pos.CENTER);
+	      
+	      
+	      VBox leftHeadings = new VBox(15);
+	      leftHeadings.getChildren().addAll(emailLabel, phoneNumberLabel, usernameLabel, passLabel, confirmPassLabel);
+	      
+	      VBox rightHeadings = new VBox(5);
+	      rightHeadings.getChildren().addAll(emailField, phoneNumberField, usernameField, passField, confirmPassField);
+	      
+	      
+	      HBox userPassConfirm = new HBox(5);
+	      userPassConfirm.getChildren().addAll(leftHeadings, rightHeadings);
+	      userPassConfirm.setAlignment(Pos.CENTER);
+	      
+	      signUpButton = new Button("Sign Up");
+	      signUpButton.setAlignment(Pos.CENTER_RIGHT);
+	      
+	   // Set up event handler for sign up button
+	        signUpButton.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent event) {
+	                // Call authenticate() method when sign up button is pressed
+	            	createNewAuthenticate();
+	            }
+	        });
+	      
+	      errMsg = new Label();
+	      errMsg.setTextFill(Color.RED);
+	      
+	      VBox root = new VBox(10);
+	      root.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+	      errMsg = new Label();
+	      errMsg.setTextFill(Color.RED);
+	      
+	      root.getChildren().addAll(imageView, heading, firstLastField, userPassConfirm, signUpButton, errMsg);
+	      root.setAlignment(Pos.CENTER);
+	      root.setPadding(new Insets(20, 20, 20, 20));
+	      
+	      scene = new Scene(root, 400, 600);
+    }
+	public Scene getScene() {return scene;}
+	
+	public boolean createNewAuthenticate() {
+  		//TODO: find stuff in authentication database
+  		if (firstNameField.getText().isBlank() || passField.getText().isBlank() 
+  				|| emailField.getText().isBlank() || phoneNumberField.getText().isBlank()
+  				|| usernameField.getText().isBlank() || lastNameField.getText().isBlank()
+  				|| confirmPassField.getText().isBlank()) {
+  			errMsg.setText("A field is empty!");
+  			return false;
+  		}
+  		errMsg.setText("");
+  		return true;
+      }
+	
+	//instance of database
+	//verify all fields are filled, error message: not all fields filled
+	//direct to patient portal
+}
