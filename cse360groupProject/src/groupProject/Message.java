8191afd543cae719e2ecc1a29e1fb425177fec1d
adrianzhang1 <53829341+adrianzhang1@users.noreply.github.com>
2024-04-10 00:59:23 -0700

    Update Message.java

--- cse360groupProject/src/groupProject/Message.java
+++ cse360groupProject/src/groupProject/Message.java
@@ -1,15 +1,18 @@
 package groupProject;
 
-public class Message {
-	private String sender; //username of sender
-	private String patient; //username of patient. If patient is the sender, patient==sender
+import java.io.Serializable;
+
+public class Message implements Serializable{
+	private static final long serialVersionUID = 12312312;
+	private Account sender; //username of sender
+	private Account patient; //username of patient. If patient is the sender, patient==sender
 	private String txt;
-	public Message(String sender, String patient, String txt) {
+	public Message(Account sender, Account patient, String txt) {
 		this.sender=sender;
 		this.patient = patient;
 		this.txt = txt;
 	}
-	public String getSender() { return sender; }
-	public String getPatient() { return patient; }
+	public Account getSender() { return sender; }
+	public Account getPatient() { return patient; }
 	public String getMsg() { return txt; }
 }
