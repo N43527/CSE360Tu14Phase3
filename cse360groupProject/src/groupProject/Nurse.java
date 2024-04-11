e2448a702e520938d0bebb58c75bd4fd6e1e8575
ysingh0603 <98730091+ysingh0603@users.noreply.github.com>
2024-04-10 21:03:47 -0700

    Update Nurse.java

--- cse360groupProject/src/groupProject/Nurse.java
+++ cse360groupProject/src/groupProject/Nurse.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 
 public class Nurse extends Account implements Serializable {
 	private static final long serialVersionUID = 2;
-	public Nurse(String _username, String _password, char _role, String _firstName, String _lastName) {
-		super(_username, _password, _role, _firstName, _lastName);
+	public Nurse(String _username, String _password, char _role, String _firstName, String _lastName, String _birthday) {
+		super(_username, _password, _role, _firstName, _lastName, _birthday);
 	}
 	public boolean isNurse() {return true;}
 }
