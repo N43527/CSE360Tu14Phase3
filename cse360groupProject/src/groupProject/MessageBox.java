a761a2bfc431170738a1cfc805828b458e66bd2b
adrianzhang1 <53829341+adrianzhang1@users.noreply.github.com>
2024-04-10 23:45:50 -0700

    Update MessageBox.java

--- cse360groupProject/src/groupProject/MessageBox.java
+++ cse360groupProject/src/groupProject/MessageBox.java
@@ -13,49 +13,50 @@ import javafx.scene.text.Text;
 
 public class MessageBox {
 	private VBox box;
-	private Button submitBttn;
+//	private Button submitBttn;
 	private TextField msgField;
-	public MessageBox(ArrayList<Message> msgs) {
+	private VBox msgPane;
+	public MessageBox() {
 	      box = new VBox(10);
 	      box.setAlignment(Pos.CENTER);
-	      System.out.println(msgs.size());
-	      VBox msgPane = new VBox();
+//	      System.out.println(msgs.size());
+	      msgPane = new VBox();
 	      msgPane.setStyle("-fx-border-color: black");
-	      submitBttn = new Button("Submit Message");
+//	      submitBttn = new Button("Submit Message");
 	      msgField = new TextField("");
 	      msgField.setAlignment(Pos.BOTTOM_CENTER);
 	      
-	      for (Message m:msgs) {
-	    	  Account s = m.getSender();
-	    	  String title = "";
-	    	  switch (s.getRole()) {
-	    	  case 'p':
-	    		  title = "Patient";
-	    		  break;
-	    	  case 'd':
-	    		  title = "Doctor";
-	    		  break;
-	    	  case 'n':
-	    		  title = "Nurse";
-	    		  break;
-	    	  }
-	    	  
-    		  Label lbl = new Label(title + " " + s.getFName() + " "+ s.getLName());
-    		  Text msg = new Text("\t" + m.getMsg() + "\n");
-    		  msgPane.getChildren().addAll(lbl,msg);
-	      }
+//	      for (Message m:msgs) {
+//	    	  Account s = m.getSender();
+//	    	  String title = "";
+//	    	  switch (s.getRole()) {
+//	    	  case 'p':
+//	    		  title = "Patient";
+//	    		  break;
+//	    	  case 'd':
+//	    		  title = "Doctor";
+//	    		  break;
+//	    	  case 'n':
+//	    		  title = "Nurse";
+//	    		  break;
+//	    	  }
+//	    	  
+//    		  Label lbl = new Label(title + " " + s.getFName() + " "+ s.getLName());
+//    		  Text msg = new Text("\t" + m.getMsg() + "\n");
+//    		  msgPane.getChildren().addAll(lbl,msg);
+//	      }
 	      
 	      msgPane.setPrefHeight(300);
 	      msgField.setPrefHeight(50);
-	      box.getChildren().addAll(msgPane,msgField,submitBttn);
+	      box.getChildren().addAll(msgPane,msgField);
 	}
 	public VBox createBox(ArrayList<Message> msgs) {
 	      box = new VBox(10);
 	      box.setAlignment(Pos.CENTER);
-	      System.out.println(msgs.size());
-	      VBox msgPane = new VBox();
+//	      System.out.println(msgs.size());
+	      msgPane = new VBox();
 	      msgPane.setStyle("-fx-border-color: black");
-	      submitBttn = new Button("Submit Message");
+//	      submitBttn = new Button("Submit Message");
 	      msgField = new TextField("");
 	      msgField.setAlignment(Pos.BOTTOM_CENTER);
 	      
@@ -81,11 +82,30 @@ public class MessageBox {
 	      
 	      msgPane.setPrefHeight(300);
 	      msgField.setPrefHeight(50);
-	      box.getChildren().addAll(msgPane,msgField,submitBttn);
+	      box.getChildren().addAll(msgPane,msgField);
 	      return box;
 	}
 	public VBox getBox() {return box;}
-	public Button getButton() {return submitBttn;}
+	//public Button getButton() {return submitBttn;}
 	public String getMessageSent() {return msgField.getText();}
 	public TextField getMsgField() {return msgField;}
+	public void addMessage(Message m) {
+		Account s = m.getSender();
+  	  String title = "";
+  	  switch (s.getRole()) {
+  	  case 'p':
+  		  title = "Patient";
+  		  break;
+  	  case 'd':
+  		  title = "Doctor";
+  		  break;
+  	  case 'n':
+  		  title = "Nurse";
+  		  break;
+  	  }
+  	  
+		  Label lbl = new Label(title + " " + s.getFName() + " "+ s.getLName());
+		  Text msg = new Text("\t" + m.getMsg() + "\n");
+		  msgPane.getChildren().addAll(lbl,msg);
+	}
 }
