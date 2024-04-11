3b42505e24ad96f542bfd44f189e11004acda765
ysingh0603 <98730091+ysingh0603@users.noreply.github.com>
2024-04-10 21:08:29 -0700

    Update Doctor.java

--- cse360groupProject/src/groupProject/Doctor.java
+++ cse360groupProject/src/groupProject/Doctor.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 
 public class Doctor extends Account implements Serializable {
 	private static final long serialVersionUID = 1;
-	public Doctor(String _username, String _password, char _role, String _firstName, String _lastName) {
-		super(_username, _password, _role, _firstName, _lastName);
+	public Doctor(String _username, String _password, char _role, String _firstName, String _lastName, String _birthday) {
+		super(_username, _password, _role, _firstName, _lastName,  _birthday);
 	}
 	public boolean isDoctor() {return true;}
 }
