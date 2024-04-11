1a21037df631552190328e4f365061ea537f7d6e
ysingh0603 <98730091+ysingh0603@users.noreply.github.com>
2024-04-10 20:59:00 -0700

    Update Account.java

--- cse360groupProject/src/groupProject/Account.java
+++ cse360groupProject/src/groupProject/Account.java
@@ -16,25 +16,28 @@ public class Account implements Serializable{
 		fName = null;
 		lName = null;
 	}
-	public Account (String _username, String _password, char _role, String _firstName, String _lastName) {
+	public Account (String _username, String _password, char _role, String _firstName, String _lastName, String _birthday) {
 		username = _username;
 		password = _password;
 		role = _role;
 		fName = _firstName;
 		lName = _lastName;
+		birthday = _birthday;
 	}
-	public void update(String _username, String _password, char _role, String _firstName, String _lastName) {
+	public void update(String _username, String _password, char _role, String _firstName, String _lastName, String _birthday) {
 		username = _username;
 		password = _password;
 		role = _role;
 		fName = _firstName;
 		lName = _lastName;
+		birthday = _birthday;
 	}
 	public String getUsername() { return username; }
 	public String getPassword() { return password; }
 	public char getRole() { return role; }
 	public String getFName() { return fName; }
 	public String getLName() { return lName; }
+	public String getBirthday() { return birthday; }
 	public boolean isNurse() { return role=='n'; }
 	public boolean isDoctor() { return role=='d'; }
 	public boolean isPatient() { return role=='p'; }
